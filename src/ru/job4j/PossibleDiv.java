ce495afcb0cac24b9aaf10e1fe695aeed1b8141f
Denis Chernenko <denis.tschernenko@gmail.com>
2020-08-16 19:10:54 +0530

    4.1. Операторы ветвлений [#343915]

---
+++ src/ru/job4j/PossibleDiv.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+class AlertDivisionByZero {
+    public static void main(String[] args) {
+        possibleDiv(4);
+        possibleDiv(0);
+        possibleDiv(-15);
+    }
+
+    public static void possibleDiv(int number) {
+        if (number >= 0) {
+            System.out.println("OK, that is possible to perform.");
+        }
+        if (number == 0) {
+            System.out.println("No, we cannot divide by zero.");
+        }
+        if (number <= 0) {
+            System.out.println("This is negative number.");
+        }
+    }
+}
