36a298ffd12ca4173ee017523e733bd7d8ba8dfc
Denis Chernenko <denis.tschernenko@gmail.com>
2020-08-17 18:56:39 +0530

    4.3.2. Оператор || [#343908]

---
+++ src/ru/job4j/Club.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+public class Club {
+    public static void permission(boolean hasMoney, boolean beFriend) {
+        boolean pass = hasMoney || beFriend;
+        if ( pass = true  ) {
+            System.out.println("I can go to the club.");
+        } else {
+            System.out.println("I can't go to the club.");
+        }
+    }
+
+    public static void main(String[] args) {
+        permission(true, true);
+        permission(true, false);
+        permission(false, true);
+        permission(false, false);
+    }
+}
