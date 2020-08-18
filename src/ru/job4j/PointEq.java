f768b5cf4e42c3db4f9ffa116fda673f240a5ee2
Denis Chernenko <denis.tschernenko@gmail.com>
2020-08-18 13:37:21 +0530

    4.3.4. if c return [#343909]

--- src/ru/job4j/PointEq.java
+++ src/ru/job4j/PointEq.java
@@ -5,11 +5,7 @@ public class PointEq {
     public static boolean eq(int x1, int y1, int x2, int y2) {
         boolean eqX = x1 == x2;
         boolean eqY = y1 == y2;
-        if (eqX && eqY) {
-            return true;
-        } else {
-            return false;
-        }
+        return eqX = eqY = true;
     }
 
     public static void main(String[] args) {
