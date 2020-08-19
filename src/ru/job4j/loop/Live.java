6d24f9d0eb6d87372cae881a8dc0e72b1beea4b3
Denis Chernenko <denis.tschernenko@gmail.com>
2020-08-19 11:56:53 +0530

    5.9.1. Генерация конструкций в IDEA [#344013]

--- src/ru/job4j/loop/Live.java
+++ src/ru/job4j/loop/Live.java
@@ -1,6 +1,6 @@
 package ru.job4j.loop;
 
-public class live {
+public class Live {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             System.out.println();
