41364625128b8e9fddea8d3fe9830549b38098d4
Denis Chernenko <denis.tschernenko@gmail.com>
2020-08-19 16:12:58 +0530

    6.0.2. Заполнение массива. [#343933]

---
+++ src/ru/job4j/array/ArrayDefinition2.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class ArrayDefinition2 {
+    public static void main(String[] args) {
+        String[] names = new String[4];
+        names[0] = "Ivan Markov";
+        names[1] = "Andrey Sergeev";
+        names[2] = "Oleg Panov";
+        names[3] = "Stanislav Berg";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+    }
+}
