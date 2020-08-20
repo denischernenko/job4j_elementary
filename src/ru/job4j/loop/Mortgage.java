06954519389befd9ecf3d3fba6cbfd8cfdbbeac7
Denis Chernenko <denis.tschernenko@gmail.com>
2020-08-20 12:47:12 +0530

    5.7. Ипотека [#344008]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,17 +3,17 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(int amount, int salary, int percent) {
         int year = 0;
-        while ((salary - amount * percent / 100) < 0) {
+        while (amount > 0) {
             amount += amount * percent / 100;
             amount -= salary;
             year += 1;
-            return year;
         }
         return year;
     }
 
     public static void main(String[] args) {
         System.out.println(Mortgage.year(100, 120, 50));
-        System.out.println(Mortgage.year(1000, 120, 50));
+        System.out.println(Mortgage.year(200, 120, 50));
+        System.out.println(Mortgage.year(300, 220, 50));
     }
 }
