26af7986ca65d4669a8256e79666477847357e1d
Denis Chernenko <denis.tschernenko@gmail.com>
2020-08-20 13:42:35 +0530

    6.1. Классический поиск перебором. [#343919]

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -1,8 +1,7 @@
 package ru.job4j.array;
 
-import javax.print.attribute.standard.PresentationDirection;
-
 public class FindLoop {
+
     public static int indexOf(int[] data, int el) {
         int result = 0; //if there is no such element in array
         for (int index = 0; index < data.length; index++) {
@@ -15,6 +14,7 @@ public class FindLoop {
     }
 
     public static void main(String[] args) {
-        System.out.println(FindLoop.indexOf(3, 3));
+        int[] data = new int[]{1, 4, 8, 10, 19};
+        System.out.println(FindLoop.indexOf(data, 3);
     }
 }
